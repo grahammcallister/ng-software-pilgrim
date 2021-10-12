1a49185ee21040c2af64d8798f42845cc6f4e37b
Graham Mc Allister <grahammcallister@gmail.com>
2021-10-12 05:38:17 +0200

    Add push-all script

---
+++ scripts/push-all.ps1
@@ -0,0 +1,37 @@
+param($commit)
+# This script pushes a specific commit to all using cherry-pick
+# Be careful there are no merge conflicts
+#
+# Usage:
+#   ./push-all.ps1 <commit>
+
+Write-Host "Cherry pick commit ${commit} to all branches..."
+
+git checkout the-software-pilgrim
+cherry-pick $commit
+git push
+
+git checkout software-pilgrim
+cherry-pick $commit
+git push
+
+git checkout software-pilgrim-com
+cherry-pick $commit
+git push
+
+git checkout the-software-pilgrim-com
+cherry-pick $commit
+git push
+
+git checkout software-pilgrim-coza
+cherry-pick $commit
+git push
+
+git checkout the-software-pilgrim-coza
+cherry-pick $commit
+git push
+
+git checkout grahammcallister-coza
+cherry-pick $commit
+git push
+
