cd3511030a5500a4a2de5294649f83d313d5bb1f
Graham <grahammcallister@gmail.com>
2023-05-26 09:59:04 +0200

    Improve push script

--- scripts/push-all.ps1
+++ scripts/push-all.ps1
@@ -1,40 +1,65 @@
 param($commit)
-# This script pushes a specific commit to all using cherry-pick
-# Be careful there are no merge conflicts
-#
+# This script pushes a specific commit to specific branches using cherry-pick
+# The script stops if there are any failed exit codes or errors
 # Usage:
 #   ./push-all.ps1 <commit>
+
+# Checks out a given branch
+Function Checkout($branch) {
+    git checkout $branch
+   }
+
+# Function to cherry pick a specific commit
+Function CherryPick($commit) {
+    git cherry-pick $commit
+}
+
+# Function to check error codes and last exit code, and exit if there is an error
+Function CheckForErrors() {
+    if ($LASTEXITCODE -ne 0) {
+        Write-Host "Exiting due to exit code ${LASTEXITCODE}"
+        exit 1
+    }
+}
+
+# Function to check the status of the current branch, and see if it is clean
+Function CheckStatus(){
+    $status = git status
+    if ($status -notlike "*nothing to commit, working tree clean*") {
+        Write-Host "Exiting due to unclean working tree"
+        exit 1
+    }
+}
 
 Write-Host "Cherry pick commit ${commit} to all branches...?"
 [void][System.Console]::ReadKey($true)
 Write-Host
 
-git checkout the-software-pilgrim
-git cherry-pick $commit
-git push
+$branches = @(
+    "software-pilgrim"
+    "the-software-pilgrim"
+    "software-pilgrim-com"
+    "the-software-pilgrim-com"
+    "software-pilgrim-coza"
+    "the-software-pilgrim-coza"
+    "grahammcallister-coza"
+)
 
-git checkout software-pilgrim
-git cherry-pick $commit
-git push
+# For each branch in branches, checkout the branch, cherry pick the commit, and push
+foreach ($branch in $branches) {
+    Write-Host "Checking out branch ${branch}..."
+    Checkout $branch
+    CheckForErrors
 
-git checkout software-pilgrim-com
-git cherry-pick $commit
-git push
+    CheckStatus
 
-git checkout the-software-pilgrim-com
-git cherry-pick $commit
-git push
+    Write-Host "Cherry picking commit ${commit}..."
+    CherryPick $commit
+    CheckForErrors
 
-git checkout software-pilgrim-coza
-git cherry-pick $commit
-git push
-
-git checkout the-software-pilgrim-coza
-git cherry-pick $commit
-git push
-
-git checkout grahammcallister-coza
-git cherry-pick $commit
-git push
+    Write-Host "Pushing branch ${branch}..."
+    git push
+    CheckForErrors
+}
 
 git checkout master
